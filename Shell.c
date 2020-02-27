80393140bd253040547b9b520c62350e54510cf6
cdk53 <cdk53@zips.uakron.edu>
2020-02-26 19:47:51 -0500

    Add basm and blackdos defines

--- Shell.c
+++ Shell.c
@@ -1,13 +1,22 @@
-
+#include "blackdos.h"
 
 int compareStrings(char*, char*);
+void subString(char*, char*, int, int);
 
 void main() {
     char input[100];
-
+    char command[100];
+    int indexOfSpace;
     while(1) {
-        interrupt(33,0,"~(__^> \0",0 ,0);
-        interrupt(33, 1, input, 0, 0);
+        PRINTS("~(__^> \0");
+        SCANS(input);
+        PRINTS(input);
+
+        // Find the first space after the command
+        indexOfSpace = 0;
+        while(input[indexOfSpace] != ' ' && input[indexOfSpace] != '\0') {
+            ++indexOfSpace;
+        }
     }
 }
 
@@ -27,4 +36,28 @@ int compareStrings(char* stringOne, char* stringTwo) {
     } else {
         return 0;
     }
+}
+
+void subString(char* givenString, char* theSubString, int startPos, int endPos) {
+    int index = startPos;
+    int subStringIndex = 0;
+
+    if(startPos < 0) {
+        interrupt(33,0,"ERROR: Substring out of bounds\r\n\0",0,0);
+        return;
+    }
+
+    while(index <= endPos) {
+        if(givenString[index] != '\0')
+        {
+            theSubString[subStringIndex] = givenString[index];
+            ++index;
+            ++subStringIndex;
+        } else {
+            interrupt(33,0,"ERROR: Substring out of bounds\r\n\0",0,0);
+            return;
+        }
+    }
+    theSubString[subStringIndex] = '\0';
+
 }
